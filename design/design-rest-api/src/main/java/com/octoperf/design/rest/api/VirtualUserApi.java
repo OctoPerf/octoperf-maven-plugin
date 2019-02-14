41a348a63bd06f7d35d9269a7d1fb251ea62da0a
jloisel <loisel.jerome@gmail.com>
2019-02-14 17:29:47 +0100

    missing annotation

--- design/design-rest-api/src/main/java/com/octoperf/design/rest/api/VirtualUserApi.java
+++ design/design-rest-api/src/main/java/com/octoperf/design/rest/api/VirtualUserApi.java
@@ -3,6 +3,7 @@ package com.octoperf.design.rest.api;
 import com.octoperf.entity.design.VirtualUser;
 import com.octoperf.entity.design.VirtualUserDescription;
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
@@ -19,5 +20,6 @@ public interface VirtualUserApi {
   @GET("/design/virtual-users/description/by-project/{projectId}")
   Call<List<VirtualUserDescription>> listDescriptions(@Path("projectId") String projectId);
 
+  @DELETE("/design/virtual-users/{id}")
   Call<Void> delete(@Path("id") String id);
 }
