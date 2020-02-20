9252c43a6a67d959fcfb294d97d024661ce5bc7c
jloisel <loisel.jerome@gmail.com>
2020-02-20 16:12:54 +0100

    fix default report url

--- maven/maven-rest/src/main/java/com/octoperf/maven/rest/RestBenchReports.java
+++ maven/maven-rest/src/main/java/com/octoperf/maven/rest/RestBenchReports.java
@@ -19,8 +19,6 @@ import static org.apache.commons.lang3.StringUtils.removeEnd;
 @AllArgsConstructor(access = PACKAGE)
 @FieldDefaults(level = PRIVATE, makeFinal = true)
 final class RestBenchReports implements BenchReports {
-  private static final String DEFAULT_API_URL = "https://api.octoperf.com";
-  private static final String SAAS_APP = "https://app.octoperf.com";
   private static final String REPORT_URL = "/#/app/workspace/%s/project/%s/analysis/%s";
 
   @Override
@@ -28,7 +26,7 @@ final class RestBenchReports implements BenchReports {
     final String serverUrl,
     final String workspaceId,
     final BenchReport report) {
-    final String baseUrl = Objects.equals(serverUrl, DEFAULT_API_URL) ? SAAS_APP : (removeEnd(serverUrl, "/") + "/app");
+    final String baseUrl = removeEnd(serverUrl, "/") + "/app";
     return String.format(
       baseUrl + REPORT_URL,
       workspaceId,
